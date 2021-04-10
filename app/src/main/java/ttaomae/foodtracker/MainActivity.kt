32a2ce02de929126be035074b5ac885fc15c4d77
Todd Taomae <ttaomae@gmail.com>
2021-04-09 17:24:34 -1000

    Update app bar. Add up navigation; update labels.

--- app/src/main/java/ttaomae/foodtracker/MainActivity.kt
+++ app/src/main/java/ttaomae/foodtracker/MainActivity.kt
@@ -2,12 +2,23 @@ package ttaomae.foodtracker
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.topAppBar))
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
